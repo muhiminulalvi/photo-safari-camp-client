4f7f98cee7754ce83e51523a378b374af4b21ae4
muhiminulalvi <eee1916039brur@gmail.com>
2023-06-07 03:06:17 +0600

    header and footer added

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,11 +1,22 @@
 
-
 const Footer = () => {
-    return (
-        <div>
-            This is footer
-        </div>
-    );
+  return (
+    <footer className="footer footer-center py-10 bg-primary ">
+      <div className="">
+        <h2 className="font-bold text-3xl text-secondary">PHOTO SAFARI CAMP</h2>
+        <p className="font-bold text-secondary">
+          Teaching reliable photography  since 2023
+        </p>
+        <p className="font-bold text-secondary">
+          Email: contact@photosafaricamp.com
+        </p>
+        <p className="font-bold text-secondary">
+          Mobile: +880 14043-73668
+        </p>
+        <p className="text-secondary font-bold">Copyright © 2023 - All right reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
